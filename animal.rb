bd5d69d2a212cf0973d06d827e65fa3ac2d8d2e5
ydarbleoj <brady.joel@gmail.com>
2014-01-11 15:21:19 -0800

    first commit

--- animal.rb
+++ animal.rb
@@ -1,11 +1,8 @@
-# what do you want to pass into the client list 
-#  an object is a combination of state and methods that use that state 
-# Phase 1 
-# Can create animals and clients
-
+require './client'
+require './shelter'
 
 class Animal
-  @@number_of_pets = 0
+
   attr_accessor :name, :age, :gender, :species, :toys
 
   def initialize( name, age, gender, species, toys )
@@ -19,29 +16,16 @@ class Animal
 end 
 
 
-class Client
-
-  attr_accessor :name, :age, :number_children, :number_pets
-
-  def initialize(name, age, number_children, number_pets)
-    @name = name 
-    @age = age
-    @number_children = number_children
-    @number_pets = number_pets
-  end 
-
-end 
-
-
-
   def menu prompt 
     puts 'clear'
     puts ":: Welcome to Happi Tails Shelter ::"
     
     puts "#{prompt}\n\n" unless prompt.empty?
 
-    puts '1 : Add animal'
-    puts '2 : Add client'
+    puts '1 : Display animals: '
+    puts '2 : Display clients: '
+    puts '3 : Add animal'
+    puts '4 : Add client'
     puts "q : Quit\n\n"
     print '-->'
     gets.chomp 
@@ -51,19 +35,14 @@ end
   while choice != 'q'
     prompt = ""
     case choice 
-    when "1"
+    when "3"
       puts "Enter animal info: "
-      print "Name: " 
-        if gets.chomp != '' 
-          prompt +=  "I did not understand ..."
-        else 
-          name = gets.chomp
-        end 
+      print "Name: "; name = gets.chomp 
       print "Age: " ; age = gets.chomp
       print "Gender: "; gender = gets.chomp
       print "Species: "; species = gets.chomp
       print "Toys: "; toys = gets.chomp 
-    when "2"
+    when "4"
       print "Enter client info: "
       print "Name: "; name = gets.chomp
       print "Age: "; age = gets.chomp
