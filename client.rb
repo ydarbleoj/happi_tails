c62d94b1a2dc55540f5d92d8962173e7579fe2ef
ydarbleoj <brady.joel@gmail.com>
2014-01-11 15:21:58 -0800

    first commit

---
+++ client.rb
@@ -0,0 +1,15 @@
+require './animal'
+require './shelter'
+
+class Client 
+
+  attr_accessor :name, :age, :number_children, :number_pets
+
+  def initialize(name, age, number_children, number_pets)
+    @name = name 
+    @age = age
+    @number_children = number_children
+    @number_pets = number_pets
+  end 
+
+end 
