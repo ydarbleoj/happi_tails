f5eeeda89a5841846cdfae4bcef5838928fb886a
ydarbleoj <brady.joel@gmail.com>
2014-01-11 16:06:01 -0800

    new file added

--- animal.rb
+++ animal.rb
@@ -1,8 +1,8 @@
 require './client'
 require './shelter'
+require './main'
 require 'pry'
 
-shelter = Shelter.new('Joel', 'Otis')
 
 class Animal
 
@@ -18,49 +18,7 @@ class Animal
 
 end 
 
-animal = Animal.new('Otis', 5, 'Male', 'Canis', 'ball')
 
-  def menu prompt 
-    puts 'clear'
-    puts ":: Welcome to Happi Tails Shelter ::"
-    
-    puts "#{prompt}\n\n" unless prompt.empty?
-
-    puts '1 : Display animals: '
-    puts '2 : Display clients: '
-    puts '3 : Add animal'
-    puts '4 : Add client'
-    puts "q : Quit\n\n"
-    print '-->'
-    gets.chomp 
-  end 
-  prompt = ""
-  choice = menu prompt 
-  while choice != 'q'
-    prompt = ""
-    case choice 
-    when "3"
-      puts "Enter animal info: "
-      print "Name: "; name = gets.chomp 
-      print "Age: " ; age = gets.chomp
-      print "Gender: "; gender = gets.chomp
-      print "Species: "; species = gets.chomp
-      print "Toys: "; toys = gets.chomp
-
-      shelter.animals << Animal.new(name, age, gender, species, toys)
-     
-    when "4"
-      print "Enter client info: "
-      print "Name: "; name = gets.chomp
-      print "Age: "; age = gets.chomp
-      print "Number of children: "; number_children = gets.chomp 
-      print "Number of pets: "; number_pets = gets.chomp
-    else 
-      prompt += 'I did not understand ... '
-    end 
-    choice = menu prompt
-
-  end 
 
 
 
