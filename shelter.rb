2f9a8891dbb1c892f42414a01f7f35d36bdf8704
ydarbleoj <brady.joel@gmail.com>
2014-01-11 16:06:22 -0800

    new file added

--- shelter.rb
+++ shelter.rb
@@ -1,8 +1,17 @@
+require './main'
 require './animal'
 require './client'
 
 class Shelter 
 
-  # should be able to display all clients and animals 
+  # should be able to display all clients and animals
+  attr_accessor :clients, :animals
+
+  def initialize( clients, animals ) 
+    @clients = []
+    @animals = []
+  end 
+
+
 
 end 
