2dcf913c860480bb7deb1fe1d73d032e43150fe4
ydarbleoj <brady.joel@gmail.com>
2014-01-13 10:13:20 -0800

    stuff that might work

--- shelter.rb
+++ shelter.rb
@@ -7,11 +7,46 @@ class Shelter
   # should be able to display all clients and animals
   attr_accessor :clients, :animals
 
-  def initialize( clients, animals ) 
+  def initialize clients, animals 
     @clients = []
     @animals = []
   end 
 
+  # here I am trying to add clients to the shelter 
+  def get_clients 
+    return @clients.select { |i| i.}
+  end 
 
+
+  def get_animals
+    return @animals.select { |i| i.adopted? }
+  end 
+
+  def animal_access name 
+    animal_name = @animals.select { |i| name == i.name }
+    return animal_name.pop
+  end 
+
+  def client_access name 
+    client_name = @client.select { |i| name == i.name }
+    return client_name.pop
+  end 
+
+  def adopt_animal animal 
+    @animals.delete(animal)
+  end 
 
 end 
+
+
+# Phase One can create animals 
+# completed 
+
+# Phase Two clients and animals can be added to shelter 
+
+# Phase Three when creating an animal or client the user is 
+# prompted for info 
+
+# Phase Four 
+
+# output and input 
