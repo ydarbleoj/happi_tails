2f3333de25227cbc08f7e381508310c741d6048f
ydarbleoj <brady.joel@gmail.com>
2014-01-13 10:12:44 -0800

    stuff that might work

--- animal.rb
+++ animal.rb
@@ -8,13 +8,18 @@ class Animal
 
   attr_accessor :name, :age, :gender, :species, :toys
 
-  def initialize( name, age, gender, species, toys )
+  def initialize name, age, gender, species, toys 
     @name = name
     @age = age
     @gender = gender
     @species = species 
-    @toys = [] 
+    @toys = toys
   end
+
+  def adoption
+    adopted.nil?
+  end 
+
 
 end 
 
