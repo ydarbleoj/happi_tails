587b267bf83ad1647aa84c7dfc8720c615fa4ba0
ydarbleoj <brady.joel@gmail.com>
2014-01-13 10:13:04 -0800

    stuff that might work

--- client.rb
+++ client.rb
@@ -6,11 +6,21 @@ class Client
 
   attr_accessor :name, :age, :number_children, :number_pets
 
-  def initialize(name, age, number_children, number_pets)
+  # here I initialize instance variables 
+  def initialize name, age, number_children, number_pets 
     @name = name 
     @age = age
-    @number_children = []
+    @number_children = number_children
     @number_pets = []
+    @exist = exist 
+  end 
+
+  def adoption_process animal 
+    @number_pets << animal 
+  end 
+
+  def exists? 
+    @exist?
   end 
 
 end 
